4018c564f81ef575debc386839ba96de65387022
freekvlier <freek.vlier@gmail.com>
2025-04-01 18:57:49 +0200

    added env file for dockerfile building

--- Docker/run.sh
+++ Docker/run.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ -f "/.env" ]; then
+    source /.env
+fi
+
+ROOT_PASSWORD=${ROOT_PASSWORD:-"root"}
+
 start_xrdp_services() {
     rm -rf /var/run/xrdp-sesman.pid
     rm -rf /var/run/xrdp.pid
@@ -16,14 +22,14 @@ stop_xrdp_services() {
 }
 
 if id "root" &>/dev/null; then
-    echo "root:root" | chpasswd || { echo "Failed to update password."; exit 1; }
+    echo "root:${ROOT_PASSWORD}" | chpasswd || { echo "Failed to update password."; exit 1; }
 else
     if ! getent group root >/dev/null; then
         addgroup root
     fi
 
     useradd -m -s /bin/bash -g root root || { echo "Failed to create user."; exit 1; }
-    echo "root:root" | chpasswd || { echo "Failed to set password."; exit 1; }
+    echo "root:${ROOT_PASSWORD}" | chpasswd || { echo "Failed to set password."; exit 1; }
     usermod -aG sudo root || { echo "Failed to add user to sudo."; exit 1; }
 fi
 
